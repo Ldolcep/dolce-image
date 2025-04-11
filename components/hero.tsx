98ebc84974b04b01c5f208cfcf61b45f6356557f
Ldolcep <louis@groupedolce.com>
2025-04-11 13:17:48 +0200

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -116,7 +116,7 @@ export default function Hero() {
       </div>
 
       {/* Contrôles audio */}
-      <div className="absolute bottom-6 right-6 xxl:bottom-60">
+      <div className="absolute bottom-6 right-6 xxl:bottom-50">
         {/* Le conteneur qui déclenche le mouseenter/mouseleave */}
         <div
           ref={volumeContainerRef}
