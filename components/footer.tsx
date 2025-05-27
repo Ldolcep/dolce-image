f7338191d914bf7a4c268881fbe902a0ad21d71a
Ldolcep <louis@groupedolce.com>
2025-05-27 16:44:03 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -20,8 +20,8 @@ export default function Footer() {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
+                width="35"
+                height="35"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
@@ -43,8 +43,8 @@ export default function Footer() {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
+                width="35"
+                height="35"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
