9870edec5b89d9d22ecfee98a69468590a9a815d
Ldolcep <louis@groupedolce.com>
2025-05-27 16:48:41 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                strokeWidth="2"
+                strokeWidth="1"
                 strokeLinecap="round"
                 strokeLinejoin="round"
               >
