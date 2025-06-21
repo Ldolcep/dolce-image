cdcdb9214617eff68003bd37347e3105d0012fc2
Ldolcep <louis@groupedolce.com>
2025-06-21 21:20:11 +0200

    Update ProjectModalDesktop.tsx

--- components/ProjectModal/ProjectModalDesktop.tsx
+++ components/ProjectModal/ProjectModalDesktop.tsx
@@ -260,7 +260,7 @@ function ProjectModalDesktop({ project, isOpen, onClose }: ProjectModalDesktopPr
         initial={{ opacity: 0, scale: 0.95 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0.95 }}
-        transition={{ duration: 0.3, ease: 'easeOut' }}
+        transition={{ type: 'spring', damping: 25, stiffness: 300, duration: 0.35 }}
         onClick={e => e.stopPropagation()}
       >
         {/* Bouton de fermeture flottant, centre aligné au coin supérieur droit du modal */}
